1b28eb90d611985ad60865290d2fa88a69c8c6f9
duongtriny <duong.triny@gmail.com>
2023-11-09 23:23:40 +0700

    lesson1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # js-foundation-learning
-test abc
---
+++ lesson1.js
@@ -0,0 +1,103 @@
+// console.log("Hello World!");
+// {
+//   console.log("Hello World!");
+//   {
+//     console.log("Hello World!");
+//     {
+//       console.log("Hello World!");
+//     }
+//   }
+// }
+// console.log("Hello World!");
+
+// {
+//   let a=3;
+//   {
+//     console.log(a);
+//   }
+// }
+// console.log(a);
+
+// let a =null;
+// console.log(a);
+
+// let a =2;
+
+// { 
+//   var a = 7;
+// }
+// console.log(a);
+
+// var a = 7;
+// var a = 11;
+// console.log(a);
+
+// const PI = 3.14;
+
+// PI = 3;
+
+// const objA ={};
+// console.log(objA);
+// objA["name"]="Tho test";
+// console.log(objA);
+
+
+// {
+//   x =3;
+// }
+// console.log(x);
+
+// let a = 2;
+// let b = 4;
+// let c = a**b;
+// console.log(c);
+
+// let a =10;
+// let b = 10%3;
+// console.log(b);
+
+// let a=1;
+// console.log(++a);
+// console.log(a);
+
+// let a=1;
+// console.log(a--);
+// console.log(a);
+
+// let a = 3;
+// let b = 45;
+// a **= b;
+// console.log(a);
+
+// let a = 45;
+// let b = 45;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+
+// if(a=b){
+//   console.log("aaaaaa")
+// }
+
+
+// let a = "Tho";
+
+// let b=" Test";
+
+// console.log(a+b);
+
+//  a +=b;
+
+//  console.log(a);
+
+// console.log(("5"===5));
+let a = 5;
+// console.log(a%2==0 && a < 6 && a>0);
+
+// console.log(a%2==0 || a > 6 || a>0);
+
+// console.log(a%2==0 && (a < 6 || a>0));
+
+console.log(!(a>6));
