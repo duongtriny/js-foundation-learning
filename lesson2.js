1ccc40a2f888ee9bebf477611324b18c29cb0c2a
duongtriny <duong.triny@gmail.com>
2023-11-14 23:21:25 +0700

    lesson2

---
+++ lesson2.js
@@ -0,0 +1,138 @@
+// console.log("Hello guys");
+// let isMale = true;
+// let a = isMale ? "Male": "Female";
+// console.log(a);
+
+// a = undefined;
+// b= a??111;
+
+// console.log(b);
+
+// let a = 0.2;
+// let b = 0.1;
+// console.log(a+b);
+
+// console.log("20"*"10");
+
+// let b = 10;
+
+// // console.log(b/"abc");
+// console.log(typeof Infinity);
+
+// let a = new Number(10);
+// let b = new Number(10);
+
+// console.log(a===b);
+// console.log(a.valueOf()==b.valueOf());
+
+// console.log(Number.MAX_VALUE);
+
+// console.log((12.46).toFixed(1));
+
+// My name is "Duong"
+// console.log("My name is \"Duong\"");
+// console.log('abc');
+// console.log("My name is 'Duong'");
+
+// console.log("abc" == "abcd");
+
+// console.log("abcdef".substring(1,3));
+
+// console.log("abcdef".toUpperCase());
+
+// console.log("ABCDEF".toLowerCase());
+
+
+// console.log("ABCDECF".replaceAll("C", "HHH"));
+
+// {
+//   let a=2;
+
+//   {
+//     let a=5;
+//     for(let a=0; a<10; a++){
+//       let a =2;
+//       console.log(a);
+//     }
+//   }
+
+//   let h =0;
+//   let h =0;
+
+//   while(h<10){
+//     let h =10;
+//     h++;
+//   }
+
+// }
+
+// console.log("Toi 10 la  20 Toi".replace(/t|y/i, "A"));
+
+// console.log("Yoi 10 la  20 Toi".replace(/t|y|1/i, "A"));
+
+// console.log("Yoi 10 la  20 Toi".replaceAll(/t|y|1/ig, "A"));
+
+// console.log("      ABc      ".trim());
+
+// let student = {
+//   "name": "Tan",
+//   "age": 10,
+//   "gender": "female",
+//   "passed":false
+// }
+//  console.log(student);
+
+
+// let student = {
+//   name: "Tan",
+//   age: 10,
+//   gender: "female",
+//   passed: false,
+//   1:"kkkk"
+// }
+// console.log(student);
+
+// student.address = "Abc def";
+
+// console.log(student);
+
+// console.log(student.address);
+
+// console.log(student[1]);
+
+
+// console.log(Object.keys(student));
+
+// console.log(student["name"]);
+
+
+// let student = {
+//   name: "Tan",
+//   age: 10,
+//   gender: "female",
+//   passed: false,
+//   // address: "271783568",
+//   getNameAndAddress: function(){
+//     return (this.name + " " + this.address);
+//   }
+// }
+
+// student.address = "ABc e3123";
+
+// console.log(student.getNameAndAddress());
+
+// console.log(delete student.age);
+
+// console.log( student);
+// const obj = {a:3, b:5};
+// console.log(obj);
+// delete obj.a;
+// console.log(obj);
+
+// console.log(JSON.stringify(obj));
+
+// a = JSON.parse("{\"a\": 3, \"b\": 5}");
+// console.log(a);
+
+
+
